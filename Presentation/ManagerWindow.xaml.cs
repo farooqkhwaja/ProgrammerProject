1f81b12e27f26967dba990c144dd43fef3980097
FarooqKhwaja <Farooqkhoja16@gmail.com>
2024-03-10 15:52:29 +0100

    Method added to open login page back after closing manager window

--- Presentation/ManagerWindow.xaml.cs
+++ Presentation/ManagerWindow.xaml.cs
@@ -8,4 +8,49 @@ public partial class ManagerWindow : Window
     {
         InitializeComponent();
     }
+
+    private void addDanceMove_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void DeleteDanceMove_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void AddEvent_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void UpdateEvent_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void DeleteEvent_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void CursistenList_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+
+    }
+
+    private void Window_Closed(object sender, EventArgs e)
+    {
+        LoginWindow loginWindow = new LoginWindow();
+        ManagerWindow managerWindow = new ManagerWindow();
+
+        if(Window_Closed != null)
+        {
+            managerWindow.Visibility = Visibility.Visible;
+        }
+        else
+        {
+            loginWindow.Visibility = Visibility.Visible;
+        }
+    }
 }
